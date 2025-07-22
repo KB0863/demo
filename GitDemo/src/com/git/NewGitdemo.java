502fc307b6bf57a6893b7af83dc95de96bac9cb5
Kshitij B. <137674120+KB0863@users.noreply.github.com>
2025-07-22 16:05:34 +0530

    Create NewGitdemo.java

---
+++ GitDemo/src/com/git/NewGitdemo.java
@@ -0,0 +1,16 @@
+package com.git;
+
+public class NewGitdemo 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+	System.out.println("Main method in Gitdemo class of package com.git");
+	}
+public void Pull()
+  {
+  System.out.println("Pull method of Gitdemo class of package com.git");
+  }
+
+}
