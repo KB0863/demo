cf334ac5dc4193a9efa01837e5b568b8755e4321
Asus <Asus@Asus>
2025-07-22 16:00:57 +0530

    first commit Gitdemo.java!

---
+++ GitDemo/src/com/git/Gitdemo.java
@@ -0,0 +1,12 @@
+package com.git;
+
+public class Gitdemo 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+	System.out.println("Main method in Gitdemo class of package com.git");
+	}
+
+}
